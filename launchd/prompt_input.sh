3ede437bff6ada3a3a80cfa2fbdaf31ecb12b4ac
Louis Martin <louismartin@fb.com>
2018-11-27 14:01:51 +0100

    Prompt now kills previous terminals and exits after success

--- launchd/prompt_input.sh
+++ launchd/prompt_input.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+pkill -f prompt_input.py  # Kill previous prompt
 /Users/louismartin/miniconda3/bin/python3 /Users/louismartin/dev/typing-trainer/prompt_input.py
+exit  # Close terminal (on mac you must tell it to exit in the preferences: https://superuser.com/questions/160473/avoiding-process-completed-prompt-after-terminal-script-ends-on-mac-os-x)
