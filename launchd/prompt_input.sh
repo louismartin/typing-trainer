616fe0ac8a186fc2428db24d2ae21b11e53ce863
Louis Martin <louismartin@fb.com>
2019-02-06 08:08:26 +0100

    Close the macOS Terminal App on exit

--- launchd/prompt_input.sh
+++ launchd/prompt_input.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pkill -f typing_trainer.py  # Kill previous prompt
 /Users/louismartin/miniconda3/bin/python3 /Users/louismartin/dev/typing-trainer/typing_trainer.py
-exit  # Close terminal (on mac you must tell it to exit in the preferences: https://superuser.com/questions/160473/avoiding-process-completed-prompt-after-terminal-script-ends-on-mac-os-x)
+kill -9 $(ps -p $(ps -p $PPID -o ppid=) -o ppid=)  # Close the Terminal App: https://stackoverflow.com/questions/5560167/osx-how-to-auto-close-terminal-window-after-the-exit-command-executed
