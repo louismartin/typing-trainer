540667aa34d07e77437633b629365fe7024b1478
Louis Martin <louismartin@fb.com>
2019-02-08 15:16:11 +0100

    Improve mac prompts

--- launchd/prompt_input.sh
+++ launchd/prompt_input.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-pkill -f typing_trainer.py  # Kill previous prompt
-/Users/louismartin/miniconda3/bin/python3 /Users/louismartin/dev/typing-trainer/typing_trainer.py
-kill -9 $(ps -p $(ps -p $PPID -o ppid=) -o ppid=)  # Close the Terminal App: https://stackoverflow.com/questions/5560167/osx-how-to-auto-close-terminal-window-after-the-exit-command-executed
+pkill -f typing_trainer.py;  # Kill previous prompt
+# Close the Terminal App: https://stackoverflow.com/questions/5560167/osx-how-to-auto-close-terminal-window-after-the-exit-command-executed
+/Users/louismartin/miniconda3/bin/python3 /Users/louismartin/dev/typing-trainer/typing_trainer.py && kill -9 $(ps -p $(ps -p $PPID -o ppid=) -o ppid=);
+kill -9 $PPID
